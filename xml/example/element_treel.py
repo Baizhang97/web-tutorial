001501612f0be12c7047916776f6bff6540c7391
zhang-bai <baizhang1997@qq.com>
2021-06-21 12:08:25 +0800

    1

--- xml/example/element_treel.py
+++ xml/example/element_treel.py
@@ -1,10 +1,37 @@
 from lxml import etree
+
+
+def save_xml(file_name: str, tree: etree._ElementTree):
+    tree.write(file_name, encoding=tree.docinfo.encoding,
+               standalone=tree.docinfo.standalone)
+    print('Save xml OK !')
+
+
+
+
 tree = etree.parse("test.xml")
 
 root = tree.getroot()
 # etree.indent(root)
 print(etree.tostring(tree,encoding='gb2312'))
 
+suffix = '.xml'
+for i in range(1):
+    file_name = str(i) + suffix
+    save_xml(file_name, tree)
 
-def save_xml():
-    tree.write('gen_test.xml',encoding=tree.docinfo.encoding,standalone=tree.docinfo.standalone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
